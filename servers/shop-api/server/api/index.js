a321095eff00ea27333a54a222981bfceaadbdf6
sravanthi <sravanthikamani@gmail.com>
2025-09-03 19:27:29 +0530

    final output

--- servers/shop-api/server/api/index.js
+++ servers/shop-api/server/api/index.js
@@ -1,3 +1,7 @@
 import app from '../server.js'
+import { createServer } from 'http'
 
-export default app
+export default function handler(req, res) {
+  const server = createServer(app)
+  server.emit('request', req, res)
+}
